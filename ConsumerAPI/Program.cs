0e61d088ebb1530a20f77948c46bf59bd26e14b4
Carlos Viana <carlos.henrique1985@hotmail.com>
2022-05-21 12:28:43 -0300

    projeto consumidor da api

---
+++ ConsumerAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsumerAPI.Services;
+using System;
+
+namespace ConsumerAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processamento processamento = new Processamento();
+            processamento.Iniciar(args);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ConsumerAPI/Services/Processamento.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsumerAPI.Services
+{
+    class Processamento
+    {
+        static HttpClient client = new HttpClient();
+
+        public void Iniciar(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("https://localhost:44311/api/pessoa");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("");
+                if (response.IsSuccessStatusCode)
+                {
+                    var teste = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine($"Saida: {teste}");
+                }
+
+                //// Create a new product
+                //Product product = new Product
+                //{
+                //    Name = "Gizmo",
+                //    Price = 100,
+                //    Category = "Widgets"
+                //};
+
+                //var url = await CreateProductAsync(product);
+                //Console.WriteLine($"Created at {url}");
+
+                //// Get the product
+                //product = await GetProductAsync(url.PathAndQuery);
+                //ShowProduct(product);
+
+                //// Update the product
+                //Console.WriteLine("Updating price...");
+                //product.Price = 80;
+                //await UpdateProductAsync(product);
+
+                //// Get the updated product
+                //product = await GetProductAsync(url.PathAndQuery);
+                //ShowProduct(product);
+
+                //// Delete the product
+                //var statusCode = await DeleteProductAsync(product.Id);
+                //Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
